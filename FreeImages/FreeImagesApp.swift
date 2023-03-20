0e337e719acf4df88b75f3cf26587346f3054a70
Biagio <biagior00@gmail.com>
2023-03-20 15:27:39 +0100

    Initial Commit

---
+++ FreeImages/FreeImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeImagesApp.swift
+//  FreeImages
+//
+//  Created by Biagio Ricci on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FreeImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
